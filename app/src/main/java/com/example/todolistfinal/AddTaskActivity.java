18853a6b9b796ae6549c8f772b42df59a0c53a21
Natalman <natalnahm@gmail.com>
2020-12-06 15:09:01 -0600

    Added some final touch to the button in the addTaskActivity and add a new drawable for the button.

--- app/src/main/java/com/example/todolistfinal/AddTaskActivity.java
+++ app/src/main/java/com/example/todolistfinal/AddTaskActivity.java
@@ -2,12 +2,17 @@ package com.example.todolistfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    private Button mAddBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        mAddBtn = findViewById(R.id.add_btn);
     }
 }
--- app/src/main/java/com/example/todolistfinal/MainActivity.java
+++ app/src/main/java/com/example/todolistfinal/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.todolistfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    
+    //initialize
+    private FloatingActionButton mAddTaskFAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mAddTaskFAB = findViewById(R.id.add_task_FAB);
+    }
+    
+    public void addTask(View view){
+        Intent intent = new Intent(this, AddTaskActivity.class);
+        startActivity(intent);
     }
 }
